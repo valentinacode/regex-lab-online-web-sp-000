1fd476490035c08769a884ec8e211b524ef2a9a5
Valentina E <tiyahelfrih@gmail.com>
2020-07-03 23:37:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,4 +1,11 @@
 def starts_with_a_vowel?(word)
+  # (/[aeiou]/)
+
+  if word.match(/\A[aeiou]/i) == nil
+    return false
+  else
+    return true
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
